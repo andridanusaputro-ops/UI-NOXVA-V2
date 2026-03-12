753c583ee615a9d4ba5942b2b16d1225a7902ff1
andridanusaputro-ops <andridanusaputro@gmail.com>
2026-03-13 04:11:32 +0700

    Update LOGICMAINmenu.lua

--- MAINMENU/LOGICMAINmenu.lua
+++ MAINMENU/LOGICMAINmenu.lua
@@ -1,1 +1,28 @@
+-- ==========================================
+-- NOXVA HUB - LOGIC MAIN MENU (KEY SYSTEM)
+-- DEVELOPED BY DANZY
+-- ==========================================
+local MainMenuLogic = {}
+
+-- [ SETTINGAN KEY & DISCORD LU DI SINI ]
+MainMenuLogic.ValidKey = "DANZY" 
+MainMenuLogic.DiscordLink = "https://discord.gg/noxvahub" 
+
+function MainMenuLogic:VerifyKey(InputKey)
+    if InputKey == self.ValidKey then
+        return true, "Key Valid!"
+    else
+        return false, "Invalid Key!"
+    end
+end
+
+function MainMenuLogic:CopyDiscordLink()
+    if setclipboard then
+        setclipboard(self.DiscordLink)
+        return true
+    end
+    return false
+end
+
+return MainMenuLogic
 
