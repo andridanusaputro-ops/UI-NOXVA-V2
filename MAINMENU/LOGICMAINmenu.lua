87877a361eb95d731ccf327513666172e1fc1ba7
andridanusaputro-ops <andridanusaputro@gmail.com>
2026-03-13 05:33:44 +0700

    Update valid key and add game support functionality

--- MAINMENU/LOGICMAINmenu.lua
+++ MAINMENU/LOGICMAINmenu.lua
@@ -4,13 +4,28 @@
 -- ==========================================
 local MainMenuLogic = {}
 
--- [ SETTINGAN KEY & DISCORD LU DI SINI ]
-MainMenuLogic.ValidKey = "DANZY" 
+-- [ SETTINGAN KEY & DISCORD ]
+MainMenuLogic.ValidKey = "NOXVA-WANGLIN-V2" 
 MainMenuLogic.DiscordLink = "https://discord.gg/noxvahub" 
+
+-- [ DATABASE GAME YANG DI-SUPPORT ]
+MainMenuLogic.SupportedGames = {
+    [81008840993724] = "Fisch / Realpse", -- Ganti sama ID game Fisch lu
+    [2753915549] = "Blox Fruits", -- Contoh game lain
+    -- Tinggal tambahin ID game lain ke depannya di sini
+}
+
+-- Fungsi cek support game
+function MainMenuLogic:CheckSupport(PlaceId)
+    if self.SupportedGames[PlaceId] then
+        return true, self.SupportedGames[PlaceId]
+    end
+    return false, "Game Not Supported"
+end
 
 function MainMenuLogic:VerifyKey(InputKey)
     if InputKey == self.ValidKey then
-        return true, "Key Valid!"
+        return true, "Key Valid! Authenticating..."
     else
         return false, "Invalid Key!"
     end
@@ -25,4 +40,3 @@ function MainMenuLogic:CopyDiscordLink()
 end
 
 return MainMenuLogic
-
